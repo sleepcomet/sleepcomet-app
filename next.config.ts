b9571c1405985860f4d003e9572c177e70dee904
neveshardd <118855875+neveshardd@users.noreply.github.com>
2025-12-29 14:40:14 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_WEBSITE_URL || "*" },
+          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_CONSOLE_URL || "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
